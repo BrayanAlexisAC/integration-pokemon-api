59d2cd052225e474f8847099151481b3a6cf0746
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-05-09 10:54:51 -0600

    Added json properties to PokemonDTO for error to mapping data

--- src/main/java/com/integration/pokemon/api/domain/dtos/PokemonDTO.java
+++ src/main/java/com/integration/pokemon/api/domain/dtos/PokemonDTO.java
@@ -1,5 +1,6 @@
 package com.integration.pokemon.api.domain.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,9 +13,13 @@ public class PokemonDTO implements Serializable {
 
     private Integer id;
     private String name;
+    @JsonProperty("location_area_encounters")
     private String locationAreaEncounters;
+    @JsonProperty("base_experience")
     private Integer baseExperience;
+    @JsonProperty("abilities")
     private ArrayList<Object> abilities;
+    @JsonProperty("held_items")
     private ArrayList<Object> heldItems;
 
 }
