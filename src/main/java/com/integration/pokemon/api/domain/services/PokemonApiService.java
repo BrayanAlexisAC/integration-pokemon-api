2f81ede9117ef550a52e03f78511cc5bf15b58bf
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-05-10 10:43:04 -0600

    Added documentation about methods for get information about Pokemons

--- src/main/java/com/integration/pokemon/api/domain/services/PokemonApiService.java
+++ src/main/java/com/integration/pokemon/api/domain/services/PokemonApiService.java
@@ -5,11 +5,25 @@ import com.integration.pokemon.api.domain.dtos.heldItem.HeldItemDTO;
 import com.integration.pokemon.api.domain.dtos.pokemon.PokemonDTO;
 
 public interface PokemonApiService {
-
+    /**
+     * Get information about a Pokemon by name
+     * @param name String, Pokemon name
+     * @return PokemonDTO, attributes about pokemon
+     */
     PokemonDTO getPokemon(String name);
 
+    /**
+     * Get information about a Pokemon ability
+     * @param url String, URL to get information about a Pokemon ability (URL inside Pokemon abilities)
+     * @return AbilityDTO, attributes about a Pokemon ability
+     */
     AbilityDTO getAbility(String url);
 
+    /**
+     * Get information about a Held item
+     * @param url String, URL to get information about Held item (URL inside Pokemon held items)
+     * @return HeldItemDTO, attributes about a Held item
+     */
     HeldItemDTO getHeldItem(String url);
 
 }
