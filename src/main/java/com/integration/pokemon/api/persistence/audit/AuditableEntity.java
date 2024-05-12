aa4c0ded6f591aad349330840cfd66fbfc7dfe8f
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-05-12 11:22:35 -0600

    Created Object AuditableEntity, to audit all entities

---
+++ src/main/java/com/integration/pokemon/api/persistence/audit/AuditableEntity.java
@@ -0,0 +1,23 @@
+package com.integration.pokemon.api.persistence.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public class AuditableEntity {
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
