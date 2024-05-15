07bef0a4b0b930a0e4d0385c78975483ab50bebb
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-05-15 11:24:08 -0600

    Refactored the way to connect with pokemon api

--- src/main/java/com/integration/pokemon/api/persistence/services/PokemonApiService.java
+++ src/main/java/com/integration/pokemon/api/persistence/services/PokemonApiService.java
@@ -4,10 +4,35 @@ import com.integration.pokemon.api.domain.dtos.ability.AbilityDTO;
 import com.integration.pokemon.api.domain.dtos.heldItem.HeldItemDTO;
 import com.integration.pokemon.api.domain.dtos.locationAreasEncounter.LocationAreaEncounterDTO;
 import com.integration.pokemon.api.domain.dtos.pokemon.PokemonDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 public interface PokemonApiService {
+    Logger LOG = LoggerFactory.getLogger(PokemonApiService.class);
+
+    /**
+     * Connection with Pokemon API
+     * @param restTemplate RestTemplate, RestTemplate instance
+     * @param url String, API URL
+     * @param responseType Class, Class type for mapper attributes
+     * @return class specified in responseType
+     * @param <T> Generic Type, Method return any type specified in responseType
+     */
+    default <T> T pokemonApiConnection (RestTemplate restTemplate, String url, Class<T> responseType) {
+        try{
+            return restTemplate.getForObject(url, responseType);
+        } catch (RestClientException re){
+            LOG.error("Error with Pokemon API responseType:{}, message:{}, cause:{}, stacktrace:{}",
+                    responseType, re.getMessage(), re.getCause(), Arrays.toString(re.getStackTrace()));
+            return null;
+        }
+    }
+
     /**
      * Get information about a Pokemon by name
      * @param name String, Pokemon name
